0664a798320ccc63dce956d8e1171ba6a08a09a5
Vishwa Joshi <joshivishwa9@gmail.com>
2023-04-19 23:55:27 +0530

    made table

--- src/pages/Component/UserData.js
+++ src/pages/Component/UserData.js
@@ -6,11 +6,12 @@ const UserData = ({ users }) => {
                 const { id, name, email, } = curUser;
                 const { street, city, zipcode } = curUser.address;
                 return (
-                    <tr key={id}>
-                        <td>{id}</td>
-                        <td>{name}</td>
+                  
+                    <tr key={id }>
+                        <td>{id }</td>
+                        <td>{name  }</td>
                         <td>{email}</td>
-                        <td>{city},{city},{zipcode}</td>
+                        <td>{city },{city },{ zipcode}</td>
                         <td> <Link to={'userPost'}>
                             View
                         </Link></td>
